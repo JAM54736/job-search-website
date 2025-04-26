6b9c4152b66b42e8940e23730c9ca2369022fc11
Tam <167722407+JAM54736@users.noreply.github.com>
2025-04-26 23:59:56 +1000

    Update router.js

--- router.js
+++ router.js
@@ -2,6 +2,7 @@
 const routes = [
     { path: '/jobdetails', component: JobDetail}, // Route for Job Detail
     {path: '/todolist', component: ToDoList}, 
+    {path: '/applicationform', component: ApplicationForm}, // Route for Application Form
     
 ];
 
