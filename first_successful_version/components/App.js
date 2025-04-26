c0d7e2857197dc940a64600a070c68bdd75fd81a
Tam <167722407+JAM54736@users.noreply.github.com>
2025-04-26 23:58:22 +1000

    Update App.js

--- first_successful_version/components/App.js
+++ first_successful_version/components/App.js
@@ -3,7 +3,8 @@ const App = {
     template: `
         <todo-list></todo-list>
         <job-overview></job-overview>
-        <job-overview></job-overview>
+        <job-applicationform></job-applicationform>
         <router-view></router-view>
+
     `,
 };                          
